80ade7c89938145602de54c22e38104ce1943a3b
luna156 <v010309@naver.com>
2025-02-13 23:58:39 +0900

    feat: 가중치 로직 추가 및 log의 빈 문자열들 제거

--- data.js
+++ data.js
@@ -1,6 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-export const members = process.env.MEMBERS ? JSON.parse(process.env.MEMBERS) : [];
+export const members = process.env.MEMBERS
+  ? JSON.parse(process.env.MEMBERS)
+  : [];
+
+export const weights = process.env.WEIGHTS
+  ? JSON.parse(process.env.WEIGHTS)
+  : {};
+
 export const TEAM_SIZE = 4;
+
+console.log(weights);
